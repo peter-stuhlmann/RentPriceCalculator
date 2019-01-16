01d17a18ede8b1ced59aec0ebe8f6315b5af272d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-01-16 01:20:06 +0100

    Made code much cleaner; functions adjusted -> more efficient

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,75 +1,63 @@
+// general formula
+
+function rentPriceCalculator_calculate(city, PricePerSquareMeter) {
+    let GeneralRent = parseInt(document.getElementById("rpc-square-meters-input").value) * PricePerSquareMeter
+    GeneralRent = GeneralRent.toFixed(2).replace('.', ',')
+    document.getElementById("rpc-city").innerHTML = `The rent in ${city} costs about ${GeneralRent} euros.`
+}
+
+
 // cities 
 
-function rentPriceCalculator_rent_berlin() {
-    let berlin = parseInt(document.getElementById("rpc-square-meters-input").value) * 12.32
-    let rent = berlin.toFixed(2).replace('.', ',')
-    document.getElementById("rpc-city").innerHTML = `The rent in Berlin costs about ${rent} euros.`
+function rentPriceCalculator_berlin() {
+    let rent = rentPriceCalculator_calculate("Berlin", 12.32)
 }
 
-function rentPriceCalculator_rent_munich() {
-    let munich = parseInt(document.getElementById("rpc-square-meters-input").value) * 17.71
-    let rent = munich.toFixed(2).replace('.', ',')
-    document.getElementById("rpc-city").innerHTML = `The rent in Munich costs about ${rent} euros.`
+function rentPriceCalculator_munich() {
+    let rent = rentPriceCalculator_calculate("Munich", 17.71)
 }
 
-function rentPriceCalculator_rent_hamburg() {
-    let hamburg = parseInt(document.getElementById("rpc-square-meters-input").value) * 11.93
-    let rent = hamburg.toFixed(2).replace('.', ',')
-    document.getElementById("rpc-city").innerHTML = `The rent in Hamburg costs about ${rent} euros.`
+function rentPriceCalculator_hamburg() {
+    let rent = rentPriceCalculator_calculate("Hamburg", 11.93)
 }
 
-function rentPriceCalculator_rent_frankfurt() {
-    let frankfurt = parseInt(document.getElementById("rpc-square-meters-input").value) * 14.00
-    let rent = frankfurt.toFixed(2).replace('.', ',')
-    document.getElementById("rpc-city").innerHTML = `The rent in Frankfurt costs about ${rent} euros.`
+function rentPriceCalculator_frankfurt() {
+    let rent = rentPriceCalculator_calculate("Frankfurt", 14.00)
 }
 
-function rentPriceCalculator_rent_stuttgart() {
-    let stuttgart = parseInt(document.getElementById("rpc-square-meters-input").value) * 13.70
-    let rent = stuttgart.toFixed(2).replace('.', ',')
-    document.getElementById("rpc-city").innerHTML = `The rent in Stuttgart costs about ${rent} euros.`
+function rentPriceCalculator_stuttgart() {
+    let rent = rentPriceCalculator_calculate("Stuttgart", 13.70)
 }
 
-function rentPriceCalculator_rent_freiburg() {
-    let freiburg = parseInt(document.getElementById("rpc-square-meters-input").value) * 12.11
-    let rent = freiburg.toFixed(2).replace('.', ',')
-    document.getElementById("rpc-city").innerHTML = `The rent in freiburg costs about ${rent} euros.`
+function rentPriceCalculator_freiburg() {
+    let rent = rentPriceCalculator_calculate("Berlin", 12.32)
 }
 
-function rentPriceCalculator_rent_mainz() {
-    let mainz = parseInt(document.getElementById("rpc-square-meters-input").value) * 12.10
-    let rent = mainz.toFixed(2).replace('.', ',')
-    document.getElementById("rpc-city").innerHTML = `The rent in Mainz costs about ${rent} euros.`
+function rentPriceCalculator_mainz() {
+    let rent = rentPriceCalculator_calculate("Mainz", 12.10)
 }
 
-function rentPriceCalculator_rent_ingolstadt() {
-    let ingolstadt = parseInt(document.getElementById("rpc-square-meters-input").value) * 12.02
-    let rent = ingolstadt.toFixed(2).replace('.', ',')
-    document.getElementById("rpc-city").innerHTML = `The rent in Ingolstadt costs about ${rent} euros.`
+function rentPriceCalculator_ingolstadt() {
+    let rent = rentPriceCalculator_calculate("Ingolstadt", 12.02)
 }
 
-function rentPriceCalculator_rent_darmstadt() {
-    let darmstadt = parseInt(document.getElementById("rpc-square-meters-input").value) * 11.97
-    let rent = darmstadt.toFixed(2).replace('.', ',')
-    document.getElementById("rpc-city").innerHTML = `The rent in Darmstadt costs about ${rent} euros.`
+function rentPriceCalculator_darmstadt() {
+    let rent = rentPriceCalculator_calculate("Darmstadt", 11.97)
 }
 
-function rentPriceCalculator_rent_wiesbaden() {
-    let wiesbaden = parseInt(document.getElementById("rpc-square-meters-input").value) * 11.97
-    let rent = wiesbaden.toFixed(2).replace('.', ',')
-    document.getElementById("rpc-city").innerHTML = `The rent in Wiesbaden costs about ${rent} euros.`
+function rentPriceCalculator_wiesbaden() {
+    let rent = rentPriceCalculator_calculate("Wiesbaden", 11.97)
 }
-
 
 // click ... 
 
-document.querySelector('#rpc-berlin').addEventListener('click', rentPriceCalculator_rent_berlin);
-document.querySelector('#rpc-munich').addEventListener('click', rentPriceCalculator_rent_munich);
-document.querySelector('#rpc-hamburg').addEventListener('click', rentPriceCalculator_rent_hamburg);
-document.querySelector('#rpc-frankfurt').addEventListener('click', rentPriceCalculator_rent_frankfurt);
-document.querySelector('#rpc-stuttgart').addEventListener('click', rentPriceCalculator_rent_stuttgart);
-document.querySelector('#rpc-freiburg').addEventListener('click', rentPriceCalculator_rent_freiburg);
-document.querySelector('#rpc-mainz').addEventListener('click', rentPriceCalculator_rent_mainz);
-document.querySelector('#rpc-ingolstadt').addEventListener('click', rentPriceCalculator_rent_ingolstadt);
-document.querySelector('#rpc-darmstadt').addEventListener('click', rentPriceCalculator_rent_darmstadt);
-document.querySelector('#rpc-wiesbaden').addEventListener('click', rentPriceCalculator_rent_wiesbaden);
+document.querySelector('#rpc-berlin').addEventListener('click', rentPriceCalculator_berlin);
+document.querySelector('#rpc-munich').addEventListener('click', rentPriceCalculator_munich);
+document.querySelector('#rpc-hamburg').addEventListener('click', rentPriceCalculator_hamburg);
+document.querySelector('#rpc-frankfurt').addEventListener('click', rentPriceCalculator_frankfurt);
+document.querySelector('#rpc-stuttgart').addEventListener('click', rentPriceCalculator_stuttgart);
+document.querySelector('#rpc-freiburg').addEventListener('click', rentPriceCalculator_freiburg);
+document.querySelector('#rpc-mainz').addEventListener('click', rentPriceCalculator_mainz);
+document.querySelector('#rpc-ingolstadt').addEventListener('click', rentPriceCalculator_ingolstadt);
+document.querySelector('#rpc-darmstadt').addEventListener('click', rentPriceCalculator_darmstadt);
+document.querySelector('#rpc-wiesbaden').addEventListener('click', rentPriceCalculator_wiesbaden);
