fd4342d31cbe9498d06bfd5295fca92bebd716a4
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-01-16 01:42:49 +0100

    fixed a bug in rentPriceCalculator_calculate (added '=>')

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,6 +1,6 @@
 // general formula
 
-const rentPriceCalculator_calculate = (city, PricePerSquareMeter) {
+const rentPriceCalculator_calculate = (city, PricePerSquareMeter) => {
     let GeneralRent = parseInt(document.getElementById("rpc-square-meters-input").value) * PricePerSquareMeter
     GeneralRent = GeneralRent.toFixed(2).replace('.', ',')
     document.getElementById("rpc-city").innerHTML = `The rent in ${city} costs about ${GeneralRent} euros.`
@@ -9,16 +9,36 @@ const rentPriceCalculator_calculate = (city, PricePerSquareMeter) {
 
 // cities 
 
-const rentPriceCalculator_berlin = () => { let rent = rentPriceCalculator_calculate("Berlin", 12.32) }
-const rentPriceCalculator_munich = () => { let rent = rentPriceCalculator_calculate("Munich", 17.71) }
-const rentPriceCalculator_hamburg = () => { let rent = rentPriceCalculator_calculate("Hamburg", 11.93) }
-const rentPriceCalculator_frankfurt = () => { let rent = rentPriceCalculator_calculate("Frankfurt", 14.00) }
-const rentPriceCalculator_stuttgart = () => { let rent = rentPriceCalculator_calculate("Stuttgart", 13.70) }
-const rentPriceCalculator_freiburg = () => { let rent = rentPriceCalculator_calculate("Berlin", 12.32) }
-const rentPriceCalculator_mainz = () => { let rent = rentPriceCalculator_calculate("Mainz", 12.10) }
-const rentPriceCalculator_ingolstadt = () => { let rent = rentPriceCalculator_calculate("Ingolstadt", 12.02) }
-const rentPriceCalculator_darmstadt = () => { let rent = rentPriceCalculator_calculate("Darmstadt", 11.97) }
-const rentPriceCalculator_wiesbaden = () => { let rent = rentPriceCalculator_calculate("Wiesbaden", 11.97) }
+const rentPriceCalculator_berlin = () => {
+    let rent = rentPriceCalculator_calculate("Berlin", 12.32)
+}
+const rentPriceCalculator_munich = () => {
+    let rent = rentPriceCalculator_calculate("Munich", 17.71)
+}
+const rentPriceCalculator_hamburg = () => {
+    let rent = rentPriceCalculator_calculate("Hamburg", 11.93)
+}
+const rentPriceCalculator_frankfurt = () => {
+    let rent = rentPriceCalculator_calculate("Frankfurt", 14.00)
+}
+const rentPriceCalculator_stuttgart = () => {
+    let rent = rentPriceCalculator_calculate("Stuttgart", 13.70)
+}
+const rentPriceCalculator_freiburg = () => {
+    let rent = rentPriceCalculator_calculate("Berlin", 12.32)
+}
+const rentPriceCalculator_mainz = () => {
+    let rent = rentPriceCalculator_calculate("Mainz", 12.10)
+}
+const rentPriceCalculator_ingolstadt = () => {
+    let rent = rentPriceCalculator_calculate("Ingolstadt", 12.02)
+}
+const rentPriceCalculator_darmstadt = () => {
+    let rent = rentPriceCalculator_calculate("Darmstadt", 11.97)
+}
+const rentPriceCalculator_wiesbaden = () => {
+    let rent = rentPriceCalculator_calculate("Wiesbaden", 11.97)
+}
 
 
 // click ... 
